fc5aa9b0a7bd94f2e28fae2402838dbd554b0bf6
mashkarrrr <mashgerrrr@yandex.ru>
2025-12-10 13:29:00 +0300

    u

--- lab4/site/table.php
+++ lab4/site/table.php
@@ -4,27 +4,28 @@ $default_cols = 5;
 $default_rows = 7;
 $default_color = '#00ff00';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $cols = abs((int) $_POST['cols']);
-  $rows = abs((int) $_POST['rows']);
-  $color = trim(strip_tags($_POST['color']));
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $cols = abs((int) $_POST['cols']);
+    $rows = abs((int) $_POST['rows']);
+    $color = trim(strip_tags($_POST['color']));
 }
-$cols = ($cols) ? $cols : 10;
-$rows = ($rows) ? $rows : 10;
-$color = ($color) ? $color : '#ffff00';
+
+$cols = ($cols) ? $cols : $default_cols;
+$rows = ($rows) ? $rows : $default_rows;
+$color = ($color) ? $color : $default_color;
 ?>
 <!-- Область основного контента -->
 <h3>Таблица умножения</h3>
-<form action='<?= $_SERVER['REQUEST_URI'] ?>' method='POST'>
-  <label>Количество колонок: </label>
-  <br>
-  <input name='cols' type='text' value='<?= isset($_POST['cols']) ? $_POST['cols'] : '' ?>'>
-  <br>
-  <label>Количество строк: </label>
-  <br>
-  <input name='rows' type='text' value='<?= isset($_POST['rows']) ? $_POST['rows'] : '' ?>'>
-  <br>
-  <label>Цвет: </label>
+<form action='<?=$_SERVER['REQUEST_URI']?>' method='POST'>
+    <label>Количество колонок: </label>
+    <br>
+    <input name='cols' type='text' value='<?=isset($_POST['cols']) ? htmlspecialchars($_POST['cols']) : $default_cols?>'>
+    <br>
+    <label>Количество строк: </label>
+    <br>
+    <input name='rows' type='text' value='<?=isset($_POST['rows']) ? htmlspecialchars($_POST['rows']) : $default_rows?>'>
+    <br>
+    <label>Цвет: </label>
   <br>
   <input name='color' type='color' value='<?= isset($_POST['color']) ? $_POST['color'] : '#00ff00' ?>' list="listColors">
   <datalist id="listColors">
